2a95b18e911f1dd47adca1ecc38a0fe8cdc963e7
JAGADEESH D L <jagadeesh.250151@cse.ritchennai.edu.in>
2026-09-14 20:55:25 +0530

    Add Phone class with display method

---
+++ phone.java
@@ -0,0 +1,12 @@
+class Phone:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def display(self):
+        print("Brand:", self.brand)
+        print("Model:", self.model)
+
+
+p = Phone("Apple", "iPhone 15")
+p.display()
